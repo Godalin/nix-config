e63273723d1320b0076191a06a9e43c1261d7405
Linyu Yang <yly1228@foxmail.com>
2025-07-28 01:08:35 +0800

    Replace release with snapshot from emacs-mix

--- flake.nix
+++ flake.nix
@@ -101,7 +101,7 @@
 
       # Standalone Home Manager configuration for x86_64 Linux
       # home-manager switch --flake .#fiction
-      homeConfigurations.xxtemp.fiction = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."xxtemp.fiction" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs { system = "x86_64-linux"; };
         modules = [
           (
